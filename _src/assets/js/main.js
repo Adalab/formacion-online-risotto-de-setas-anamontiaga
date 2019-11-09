700df843c50f0da649a451e7bea2b3ee3a3aa4b9
anamontiaga <amontiaga@gmail.com>
2019-11-09 18:11:31 +0100

    fetch recipe

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,14 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const title = document.querySelector(".header__title");
+const getRecipe = () => {
+  fetch("https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json")
+    .then(response => response.json())
+    .then(data => {
+      const recipe = data.recipe;
+      title.innerHTML = recipe.name;
+      console.log(recipe.name);
+    });
+};
+
+getRecipe();
