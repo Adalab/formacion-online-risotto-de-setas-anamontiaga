13917d1d3910c146a194015e1515a0a1b7ecd63c
anamontiaga <amontiaga@gmail.com>
2019-11-11 17:36:16 +0100

    sum total and subtotal

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,7 +1,61 @@
 "use strict";
-import data from "./data/data.js";
+const data = {
+  recipe: {
+    name: "Risotto de setas (vegano)",
+    "shipping-cost": 7,
+    currency: "€",
+    ingredients: [
+      {
+        product: "Margarina de maíz",
+        brand: "Artua",
+        items: 1,
+        quantity: "600 gr.",
+        price: 2.95
+      },
+      {
+        product: "Arroz de Valencia",
+        brand: "De Nuestra Tierra",
+        items: 1,
+        quantity: "1 kg.",
+        price: 2.4
+      },
+      {
+        product: "Caldo de verduras natural",
+        brand: "Aneto",
+        items: 1,
+        quantity: "1 l.",
+        price: 3.6
+      },
+      {
+        product: "Seta Shiitake ecológica",
+        items: 1,
+        quantity: "200 gr.",
+        price: 3.55
+      },
+      {
+        product: "Paragoce, vino blanco",
+        brand: "Verdejo D.O. Rueda",
+        items: 1,
+        quantity: "0,57 cl.",
+        price: 5.85
+      },
+      {
+        product: "Ajo",
+        items: 1,
+        quantity: "270 gr.",
+        price: 1.49
+      },
+      {
+        product: "Cebolla chalotas",
+        items: 1,
+        quantity: "200 gr.",
+        price: 2.99
+      }
+    ]
+  }
+};
 
-console.log(data);
+// DOM
 
 const title = document.querySelector(".header__title");
 const articles = document.querySelectorAll(".main__article--description--title");
@@ -11,74 +65,78 @@ const quantities = document.querySelectorAll(".main__article--description--weigh
 const prices = document.querySelectorAll(".main__articule--description--price");
 const itemsInput = document.querySelectorAll(".items__input");
 const subtotal = document.querySelector(".footer__subtotal--number");
+const total = document.querySelector(".footer__total--number");
+const shippingCosts = document.querySelector(".footer__shippingcosts--number");
+const totalBtn = document.querySelector(".footer__btn--span");
 let totalItems = document.querySelector(".footer__items--number");
 
 const getRecipe = () => {
-  fetch(data)
-    .then(response => response.json())
-    .then(data => {
-      const recipe = data.recipe;
-      title.innerHTML = recipe.name;
-      for (const item of currency) {
-        item.innerHTML = recipe.currency;
-      }
-      const ingredients = recipe.ingredients;
+  // fetch(data)
+  //   .then(response => response.json())
+  //   .then(data => {
+  const recipe = data.recipe;
+  title.innerHTML = recipe.name;
+  for (const item of currency) {
+    item.innerHTML = recipe.currency;
+  }
+  const ingredients = recipe.ingredients;
 
-      for (let i = 0; i < ingredients.length; i = i + 1) {
-        for (let i = 0; i < articles.length; i = i + 1) {
-          articles[i].innerHTML = ingredients[i].product;
-        }
-        for (let i = 0; i < brands.length; i = i + 1) {
-          ingredients[i].brand ? (brands[i].innerHTML = ingredients[i].brand) : (brands[i].innerHTML = "");
-        }
+  for (let i = 0; i < ingredients.length; i = i + 1) {
+    for (let i = 0; i < articles.length; i = i + 1) {
+      articles[i].innerHTML = ingredients[i].product;
+    }
+    for (let i = 0; i < brands.length; i = i + 1) {
+      ingredients[i].brand ? (brands[i].innerHTML = ingredients[i].brand) : (brands[i].innerHTML = "");
+    }
 
-        for (let i = 0; i < quantities.length; i = i + 1) {
-          quantities[i].innerHTML = ingredients[i].quantity;
-        }
-        for (let i = 0; i < prices.length; i = i + 1) {
-          prices[i].innerHTML = ingredients[i].price;
-        }
-        for (let i = 0; i < itemsInput.length; i = i + 1) {
-          itemsInput[i].value = ingredients[i].items;
-        }
-      }
-    });
-  // LOADER
+    for (let i = 0; i < quantities.length; i = i + 1) {
+      quantities[i].innerHTML = ingredients[i].quantity;
+    }
+    for (let i = 0; i < prices.length; i = i + 1) {
+      prices[i].innerHTML = ingredients[i].price;
+    }
+    for (let i = 0; i < itemsInput.length; i = i + 1) {
+      itemsInput[i].value = ingredients[i].items;
+    }
+  }
 };
+// LOADER
 
 getRecipe();
 
 // SUBTOTAL
+
 const sumSubtotal = () => {
+  let acumulator = 0;
   for (let i = 0; i < prices.length; i = i + 1) {
-    console.log(prices[i]);
+    acumulator += parseFloat(prices[i].innerHTML);
   }
-  // console.log(subtotal.innerHTML);
+  subtotal.innerHTML = acumulator;
 };
 
 sumSubtotal();
 
-//MULTIPLICAR CANTIDADES POR PRECIOS
-const itemsPrice = () => {
-  for (let i = 0; i < itemsInput.length; i = i + 1) {}
+// TOTAL
+
+const sumTotal = () => {
+  total.innerHTML = parseFloat(subtotal.innerHTML) + parseFloat(shippingCosts.innerHTML);
+  totalBtn.innerHTML = parseFloat(total.innerHTML);
 };
+
+sumTotal();
 
 // SUMAR ITEMS
-let sum = 0;
-let itemsArray = [];
-const listenInput = () => {
-  for (let i = 0; i < itemsInput.length; i++) {
-    itemsArray.push(itemsInput[i].value);
+let ac = 0;
+const itemsPrice = () => {
+  for (let i = 0; i < itemsInput.length; i = i + 1) {
+    ac += parseInt(itemsInput[i].value);
   }
+  totalItems.innerHTML = ac;
 };
 
-listenInput();
-
-// sum += parseInt(itemsInput[i].value);
-//   totalItems.innerHTML = sum;
-// for (const itemInput of itemsInput) {
-//   itemInput.addEventListener("change", listenInput);
-// }
+for (const itemInput of itemsInput) {
+  itemInput.addEventListener("change", itemsPrice);
+}
 
 // SUMAR TODOS LOS PRECIOS
 const subtotalCount = () => {
